b48d126e9971297f8a7d50c69b5982963d5655e8
Qingqing Li <qqli@bu.edu>
2015-04-21 00:26:56 -0400

    addBody & getQuadrant functions

--- non-bruteforce/QuadNode.cpp
+++ non-bruteforce/QuadNode.cpp
@@ -7,12 +7,15 @@
 #include <iostream>
 #include <cmath>
 
-QuadNode::QuadNode(long double xmin, long double xmax, 
-		long double ymin, long double ymax, Body* mybody)
+QuadNode::QuadNode(long double x1, long double x2, 
+		long double y1, long double y2, long double x, long double y, long double mass, Body* mybody)
  {  xmin(xmin),
     xmax(xmax),
     ymin(ymin),
     ymax(ymax),
+    mx(x),
+    my(y),
+    m(mass),
     theta(1.0),
     isparent(false),
     me(mybody),
@@ -30,6 +33,27 @@ QuadNode::~QuadNode();
 }
 
 QuadNode::addBody(Body* body){
+	//If this quadnode does not have a body in it, just add new body and return.
+    if(me==NULL){
+    	this->me = body;
+    	this->mx = body->x;
+    	this->my = body->y;
+    	this->m = body->mass;
+    	return;
+    }
+    if(!this->isparent){
+    	this->createChildren();
+
+    	this->myChildren[this->getQuadrant(body)] = addBody(body);
+    	this->myChildren[getQuadrant(me)] = addBody(me);
+
+    	this->calcMass();
+    }else{
+    	this->myChildren[this->getQuadrant(body)] = addBody(body);
+    	this->myChildren[getQuadrant(me)] = addBody(me);
+
+    	this->calcMass();
+    }
 
 }
 
@@ -41,7 +65,7 @@ QuadNode::clearNode (){
 
 }
 
-QuadNode::recalculate(){
+QuadNode::calcMass(){
 
 }
 
@@ -54,27 +78,40 @@ QuadNode::calAllForce(BodySystem* bs){
 }
 
 QuadNode::getQuadrant(Body* body){
-
+    //0,1,2,3 means the four quadrant, 5 means that this body does not fit in this quadnode
+	if(body->x >= xmin && body->x <= (xmin+xmax)/2){
+		if(body->y >= ymin && body->y <= (ymin+ymax)/2){
+			return 2;
+		}else if(body->y > (ymin+ymax)/2 && body->y <= ymax){
+			return 0;
+		}else return 5;
+	}else if(body->x > (xmin+xmax)/2 && body->x <= xmax){
+		if(body->y >= ymin && body->y <= (ymin+ymax)/2){
+			return 3;
+		}else if(body->y > (ymin+ymax)/2 && body->y <= ymax){
+			return 1;
+		}else return 5;
+	}else return 5;
 }
 
 QuadNode::getXmin(){
-
+	return this->xmin;
 }
 QuadNode::getXmax(){
-
+	return this->xmax;
 }
 QuadNode::getYmin(){
-
+	return this->ymin;
 }
 QuadNode::getYmax(){
-
+	return this->ymax;
 }
 QuadNode::setTheta(){
-
+	return this->theta;
 }
 QuadNode::isParent(){
-
+	return this->isparent;
 }
 QuadNode::createChildren(){
-	
+
 }
--- non-bruteforce/QuadNode.hpp
+++ non-bruteforce/QuadNode.hpp
@@ -16,13 +16,8 @@ private:
 
 public:
 	//Create a quadtree with a certain space
-	QuadNode(long double xmin, long double xmax, 
-		long double ymin, long double ymax, Body* body)
-	//double xmin,xmax;
-	//double ymin,ymax;
-	//double* NE,NW,SE,SW;
-	//double mass;  //Total mass
-	//double x,y; //Center of mass
+	QuadNode(long double x1, long double x2, 
+		long double y1, long double y2, long double x, long double y, long double mass, Body* mybody)
 
     //Create a quadtree using existing file
 	QuadNode( BodySystem* bs );
@@ -40,7 +35,7 @@ public:
 	void clearNode ();
 
 	//Recalculate center of mass and total mass
-	void recalculate();
+	void calcMass();
 
 	//Calculate a single body's force
 	void calcForce(Body* body);
