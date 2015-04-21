497b1f875dc6dfafc540e31814e78e9c285ae586
Qingqing Li <qqli@bu.edu>
2015-04-21 02:00:12 -0400

    calcMass & calcForce

--- non-bruteforce/QuadNode.cpp
+++ non-bruteforce/QuadNode.cpp
@@ -50,7 +50,6 @@ QuadNode::addBody(Body* body){
     	this->calcMass();
     }else{
     	this->myChildren[this->getQuadrant(body)] = addBody(body);
-    	this->myChildren[getQuadrant(me)] = addBody(me);
 
     	this->calcMass();
     }
@@ -66,11 +65,54 @@ QuadNode::clearNode (){
 }
 
 QuadNode::calcMass(){
-
+    if(this->isparent){
+        //add up all the mass of children for quadnode mass
+        //calculate mx and my separately
+        //First set mx, my and m to zero
+        this->mx = 0.0; this->my = 0.0; this->m = 0.0;
+        for(int i = 0; i < 4; ++i){
+            if(this->myChildren[i]!=NULL){
+                //If there is a body in myChildren[i],add up the mass and mass*position
+            this->mx += this->myChildren[i]->mx * this->myChildren[i]->m; 
+            this->my += this->myChildren[i]->my * this->myChildren[i]->m;
+            this->m += this->myChildren[i]->m; 
+            }
+        }
+        this->mx = this->mx/this->m;
+        this->my = this->my/this->m;
+    }
+    if(!this->isparent){
+        return;
+    }
 }
 
 QuadNode::calcForce(Body* body){
+/*    long double dx = this->mx - body->x;
+    long double dy = this->my - body->y;
+    long double d2 = dx * dx + dy * dy;
+    long double d = sqrt(d2);
+    if(this->isparent){
+       if(d/r >= theta)//I forgot what is d and what is r
+        {//We need to separate to four smaller nodes for this quadnode and calculate recursively
+            for(int i = 0; i < 4; i++){
+                if(this->myChildren[i] != NULL){
+                   this->myChildren[i] -> calcForce(body);
+                }
+                return;
+            }
 
+        }else{//means that we need to add fx and fy variables into the function
+            //The condition that we can consider the quadnode as a whole calculating force
+            body->fx = (dx/d)* this->m * body->mass /d2;
+            body->fy = (dy/d)* this->m * body->mass /d2;
+            return;
+        }
+    }else{ //The condition that we only have one body in the quadnode
+        body->fx = (dx/d)* this->m * body->mass /d2;
+        body->fy = (dy/d)* this->m * body->mass /d2;  
+        return;      
+    }
+    */
 }
 
 QuadNode::calAllForce(BodySystem* bs){
@@ -79,16 +121,16 @@ QuadNode::calAllForce(BodySystem* bs){
 
 QuadNode::getQuadrant(Body* body){
     //0,1,2,3 means the four quadrant, 5 means that this body does not fit in this quadnode
-	if(body->x >= xmin && body->x <= (xmin+xmax)/2){
-		if(body->y >= ymin && body->y <= (ymin+ymax)/2){
+	if((body->x >= this->xmin) && (body->x <= (this->xmin + this->xmax)/2)){
+		if((body->y >= this->ymin) && (body->y <= (this->ymin + this->ymax)/2){
 			return 2;
-		}else if(body->y > (ymin+ymax)/2 && body->y <= ymax){
+		}else if((body->y > (this->ymin + this->ymax)/2) && (body->y <= this->ymax)){
 			return 0;
 		}else return 5;
-	}else if(body->x > (xmin+xmax)/2 && body->x <= xmax){
-		if(body->y >= ymin && body->y <= (ymin+ymax)/2){
+	}else if((body->x > (this->xmin + this->xmax)/2) && (body->x <= this->xmax)){
+		if((body->y >= this->ymin) && (body->y <= (this->ymin + this->ymax)/2)){
 			return 3;
-		}else if(body->y > (ymin+ymax)/2 && body->y <= ymax){
+		}else if((body->y > (this->ymin + this->ymax)/2) && (body->y <= this->ymax){
 			return 1;
 		}else return 5;
 	}else return 5;
